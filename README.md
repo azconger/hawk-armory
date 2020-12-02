1caf6239e6b31ca904b05241affcea43f8b438df
Zachary Conger <zachary.conger@stackhawk.com>
2020-12-02 16:02:47 -0700

    update docker image build script for versioning

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hawk-armory
+
+This is a custom HawkScan image and Armory Spinnaker configuration which demonstrates scanning an app as a step in a deployment pipeline.
+
--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# First arg is version, or default
+VERSION=${1:-0.3}
+IMAGE="zconger/hawkscan"
+TAG="spinnaker-${VERSION}"
+
 pushd $(git rev-parse --show-toplevel) || exit 1
 
-docker build . -t zconger/hawkscan:spinnaker-0.1
-docker push zconger/hawkscan:spinnaker-0.1
+docker build . -t "${IMAGE}:${TAG}"
+docker tag "${IMAGE}:${TAG}" "${IMAGE}:spinnaker"
+docker tag "${IMAGE}:${TAG}" "${IMAGE}:latest"
+
+docker push "${IMAGE}:${TAG}"
+docker push "${IMAGE}:spinnaker"
+docker push "${IMAGE}:latest"
 
 popd || exit 1
